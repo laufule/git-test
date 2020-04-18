44683df47b42fe9354a7d8b5f6e37658ef34b644
laufule <laufule@gmail.com>
2020-04-18 23:52:24 +0800

    change cpp file

--- hello.cpp
+++ hello.cpp
@@ -2,5 +2,9 @@
 int main()
 {
     str::cout << "hello world.";
+    int a;
+    cout << "Please input a number: ";
+    cin >> a;
+    cout << "The number you input is " << a << endl;
     return 0;
 }
