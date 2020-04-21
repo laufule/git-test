cebed0e642d55ec84accb84c813b7c9ac8330251
laufule <laufule@gmail.com>
2020-04-21 23:39:20 +0800

    add something good

--- str.py
+++ str.py
@@ -14,3 +14,14 @@ list3 = str3.split('.')
 print("list3:")
 print(list3)
 
+message = "wellcome to Beijing."
+print(message.upper())
+print(message.title())
+print(message.lower())
+
+first_name = "liu"
+last_name = "fule"
+full_name = first_name + " " + last_name
+message = "Hello " + full_name.title() + "."
+print(message)
+
