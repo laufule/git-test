f72acb371f19661808d43a1881ec0791a0e584ea
laufule <laufule@gmail.com>
2020-04-21 23:32:45 +0800

    add a str.py file

---
+++ str.py
@@ -0,0 +1,16 @@
+# str
+str1 = "123654"
+list1 = list(str1)
+print("list1:")
+print(list1)
+
+str2 = "ab cd ef"
+list2 = str2.split()
+print("list2:")
+print(list2)
+
+str3 = "www.github.com"
+list3 = str3.split('.')
+print("list3:")
+print(list3)
+
