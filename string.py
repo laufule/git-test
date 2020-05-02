d55bbe8ba2b4a7ad8e7173ce52262d192a313dc1
laufule <laufule@gmail.com>
2020-05-02 15:33:30 +0800

    change name

--- string.py
+++ string.py
@@ -1,6 +1,6 @@
 #text for string
 myStr = "fule"
 myWord = "good afternoon"
-message = myWord.title() + " "  + myStr.upper() + "."
+message = myWord.title() + " "  + myStr.title() + "."
 print(message)
 
